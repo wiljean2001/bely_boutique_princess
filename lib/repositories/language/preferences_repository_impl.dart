bf93c4184c46716d28e30849756a8a54c4f36116
wiljean2001 <86029273+wiljean2001@users.noreply.github.com>
2022-06-23 11:54:27 -0500

    change languaje solved

--- lib/repositories/language/preferences_repository_impl.dart
+++ lib/repositories/language/preferences_repository_impl.dart
@@ -1,5 +1,6 @@
 import 'dart:ui';
 import 'package:shared_preferences/shared_preferences.dart';
+
 import 'preferences_repository.dart';
 
 class PreferencesRepositoryImpl implements PreferencesRepository {
@@ -8,12 +9,12 @@ class PreferencesRepositoryImpl implements PreferencesRepository {
   static const String _localeCountryCodeKey = 'localeCountryCode';
 
   @override
-  Future<void> saveLocale(Locale? locale) async {
+  Future<void> saveLocale(Locale locale) async {
     final prefs = await SharedPreferences.getInstance();
 
-    await prefs.setString(_localeLanguageCodeKey, locale?.languageCode);
-    await prefs.setString(_localeScriptCodeKey, locale?.scriptCode);
-    await prefs.setString(_localeCountryCodeKey, locale?.countryCode);
+    await prefs.setString(_localeLanguageCodeKey, locale.languageCode);
+    await prefs.setString(_localeScriptCodeKey, locale.scriptCode!);
+    await prefs.setString(_localeCountryCodeKey, locale.countryCode!);
   }
 
   @override
